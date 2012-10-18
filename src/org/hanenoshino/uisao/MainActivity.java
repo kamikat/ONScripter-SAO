3d4e68e165c04a676eb4005e695f281f8442ea4b
trinity <imandry.c@gmail.com>
2012-10-18 13:41:24 +0800

    No title window

--- src/org/hanenoshino/uisao/MainActivity.java
+++ src/org/hanenoshino/uisao/MainActivity.java
@@ -19,6 +19,7 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
@@ -94,6 +95,7 @@ public class MainActivity extends Activity implements OnItemClickListener {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
 		findViews();
 		CoverDecoder.init(getApplicationContext(), cover.getWidth(), cover.getHeight());
