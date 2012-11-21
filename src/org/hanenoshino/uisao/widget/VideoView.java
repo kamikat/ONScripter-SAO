45e4f7a873b7a60674f3fa68aa1d3476f2f657f9
trinity <imandry.c@gmail.com>
2012-11-21 13:02:28 +0800

    MediaController anchor to rootview

--- src/org/hanenoshino/uisao/widget/VideoView.java
+++ src/org/hanenoshino/uisao/widget/VideoView.java
@@ -265,7 +265,7 @@ public class VideoView extends SurfaceView implements MediaController.MediaPlaye
 	private void attachMediaController() {
 		if (mMediaPlayer != null && mMediaController != null) {
 			mMediaController.setMediaPlayer(this);
-			View anchorView = this.getParent() instanceof View ? (View) this.getParent() : this;
+			View anchorView = this.getRootView(); // this.getParent() instanceof View ? (View) this.getParent() : this;
 			mMediaController.setAnchorView(anchorView);
 			mMediaController.setEnabled(isInPlaybackState());
 
