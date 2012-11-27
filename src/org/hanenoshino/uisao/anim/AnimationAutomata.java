34ae5e15c9f314a218bd9f17400b27a96b35eeea
trinity <imandry.c@gmail.com>
2012-11-27 09:39:16 +0800

    StateIO framework [Automata -> Observer]

--- src/org/hanenoshino/uisao/anim/AnimationAutomata.java
+++ src/org/hanenoshino/uisao/anim/AnimationAutomata.java
@@ -1,5 +1,78 @@
 package org.hanenoshino.uisao.anim;
 
-public class AnimationAutomata {
+import android.view.animation.Animation;
 
+/**
+ * Automata builder for Animation states.
+ * An automata should be attatched to a View and be passed a StateRunner.
+ * Then when StateRunner transfer states, animation should be correctly triggered.
+ * @author trinity
+ *
+ */
+public class AnimationAutomata implements StateIO {
+	
+	private final StateIO runner;
+	
+	// To avoid circular notify
+	private int lastIssue = 0;
+	
+	private AnimationAutomata(StateIO sio) {
+		runner = sio;
+	}
+	
+	public static AnimationAutomata to(StateIO sio) {
+		if(sio == null) sio = new StateRunner();
+		return new AnimationAutomata(sio);
+	}
+
+	/**
+	 * Add AutomataAction from this state to another state
+	 * @param from
+	 * @param to
+	 * @param action
+	 * @return
+	 */
+	public AnimationAutomata addAction(int from, int to, AutomataAction action) {
+		return this;
+	}
+	
+	/**
+	 * Add Animation from this state to another state
+	 * @param from
+	 * @param to
+	 * @param anim
+	 * @return
+	 */
+	public AnimationAutomata addAnimation(int from, int to, Animation anim) {
+		return this;
+	}
+
+	public void onStateTransferred(int before, int after, int issueId) {
+		if(lastIssue == issueId) return;
+		lastIssue = issueId;
+		// TODO Check state transfer and decide animation and actions to be taken
+	}
+
+	// Following code behave as a wrapper to StateIO
+	public StateIO gotoState(int to) {
+		runner.gotoState(to);
+		return this;
+	}
+
+	public int currentState() {
+		return runner.currentState();
+	}
+
+	public void addSublevelStateIO(StateIO sio) {
+		runner.addSublevelStateIO(sio);
+	}
+	
+	public boolean removeSublevelStateIO(StateIO sio) {
+		return runner.removeSublevelStateIO(sio);
+	}
+
+	public void clearSublevelStateIO() {
+		runner.clearSublevelStateIO();
+	}
+	
 }
---
+++ src/org/hanenoshino/uisao/anim/StateRunner.java
@@ -0,0 +1,70 @@
+package org.hanenoshino.uisao.anim;
+
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import android.os.Handler;
+import android.os.Message;
+
+/**
+ * StateRunner synchronized states between different AnimationAutomata
+ * StateRunner itself can transfer its state.
+ * @author trinity
+ *
+ */
+public class StateRunner implements StateIO {
+	
+	private static AtomicInteger msgCounter = new AtomicInteger(0);
+	private static Handler handler = new Handler() {
+		
+		public void handleMessage(Message msg) {
+			Object o = msg.obj;
+			if(!(o instanceof StateIO)) 
+				return;
+			StateIO io = (StateIO) o;
+			io.onStateTransferred(msg.arg1, msg.arg2, msg.what);
+		}
+		
+	};
+	
+	private int state;
+	
+	// To avoid circular notify
+	private int lastIssue = 0;
+
+	private ArrayList<StateIO> listener;
+	
+	public void addSublevelStateIO(StateIO sio) {
+		listener.add(sio);
+	}
+	
+	public boolean removeSublevelStateIO(StateIO sio) {
+		return listener.remove(sio);
+	}
+	
+	public void clearSublevelStateIO() {
+		listener.clear();
+	}
+
+	public void onStateTransferred(int before, int after, int issueId) {
+		if(lastIssue == issueId) return;
+		lastIssue = issueId;
+		for(StateIO io : listener) {
+			io.onStateTransferred(before, after, issueId);
+		}
+	}
+
+	public StateIO gotoState(int to) {
+		if(state == to) return this;
+		int pstate = state;
+		state = to;
+		handler.obtainMessage(msgCounter.incrementAndGet(), pstate, state, this);
+		return this;
+	}
+
+	@Override
+	public int currentState() {
+		return state;
+	}
+
+}
