80cf812e7d239cab18f34e6beeb68f4b6c7ec0c7
trinity <imandry.c@gmail.com>
2012-12-19 00:34:06 +0800

    button_visible and screen_centered preference

--- src/com/shinohane/onsao/GamePreference.java
+++ src/com/shinohane/onsao/GamePreference.java
@@ -13,6 +13,12 @@ public class GamePreference {
 	// Optional draw outline of text
 	public boolean draw_outline;
 	
+	// Optional whether show side buttons during game play
+	public boolean button_visible;
+	
+	// Optional whether to center screen during game play
+	public boolean screen_centered;
+	
 	public static GamePreference fromJSON(final JSONObject json) throws JSONException {
 		return new GamePreference(){{ readJSON(json); }};
 	}
@@ -22,12 +28,18 @@ public class GamePreference {
 			engine = json.getString("engine");
 		if(json.has("draw_outline"))
 			draw_outline = json.getBoolean("draw_outline");
+		if(json.has("button_visible"))
+			button_visible = json.getBoolean("button_visible");
+		if(json.has("screen_centered"))
+			screen_centered = json.getBoolean("screen_centered");
 	}
 	
 	public JSONObject toJSON() throws JSONException {
 		JSONObject b = new JSONObject();
 		b.put("engine", engine);
 		b.put("draw_outline", draw_outline);
+		b.put("button_visible", button_visible);
+		b.put("screen_centered", screen_centered);
 		return b;
 	}
 
@@ -35,6 +47,8 @@ public class GamePreference {
 		GamePreference g = new GamePreference();
 		g.engine = bundle.getString("engine");
 		g.draw_outline = bundle.getBoolean("draw_outline");
+		g.button_visible = bundle.getBoolean("button_visible");
+		g.screen_centered = bundle.getBoolean("screen_centered");
 		return g;
 	}
 	
@@ -42,6 +56,8 @@ public class GamePreference {
 		Bundle b = new Bundle();
 		b.putString("engine", engine);
 		b.putBoolean("draw_outline", draw_outline);
+		b.putBoolean("button_visible", button_visible);
+		b.putBoolean("screen_centered", screen_centered);
 		return b;
 	}
 	
